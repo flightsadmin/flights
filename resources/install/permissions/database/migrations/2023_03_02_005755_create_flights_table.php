78a214380f2e07a284b8ba8bb3116962287ee5b9
George Chitechi <52364198+georgechitechi@users.noreply.github.com>
2023-04-24 01:47:20 +0300

    Updates

--- resources/install/permissions/database/migrations/2023_03_02_005755_create_flights_table.php
+++ resources/install/permissions/database/migrations/2023_03_02_005755_create_flights_table.php
@@ -62,7 +62,7 @@ return new class extends Migration
             $table->timestamp('touchdown')->nullable();
             $table->timestamp('onblocks')->nullable();
             $table->integer('passengers')->nullable();
-            $table->string('flight_time')->nullable();
+            $table->time('flight_time')->nullable();
             $table->text('remarks')->nullable();
             $table->timestamps();
         });
